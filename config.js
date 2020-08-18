630a7da424c5d242d395731bce87a4b9217833a6
wlachoo <w.edgardo22@gmail.com>
2020-08-18 12:52:09 -0400

    make it better

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   useMongoClient: true,
   port: process.env.PORT || 3001,
-  db: process.env.MONGODB_URI || 'mongodb+srv://wlachoo:GxJb9IJ9fc9uoc3j@cluster0.ylgzx.mongodb.net/shop',
+  db: process.env.MONGODB_URI || 'mongodb+srv://wlachoo:k9WZmQWSOtBnHVOz@cluster0.ylgzx.mongodb.net/shop',
   SECRET_TOKEN: 'miclavedetokens'
 }

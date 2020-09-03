2db0299f4f1458f86631ff29ed38c5130492123f
Wladimir <w.edgardo22@gmail.com>
2020-09-02 22:12:49 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   useMongoClient: true,
   port: process.env.PORT || 3001,
-  db: process.env.MONGODB_URI || 'mongodb+srv://wlachoo:k9WZmQWSOtBnHVOz@cluster0.ylgzx.mongodb.net/shop',
+  db: process.env.MONGODB_URI || 'mongodb://localhost:27017/productsImagenes',
   SECRET_TOKEN: 'miclavedetokens'
 }
